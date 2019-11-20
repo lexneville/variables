3854482dae86c5febe10c5fe262451111d02b16f
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-20 11:56:33 +0000

    reupload

---
+++ variables.js
@@ -0,0 +1,22 @@
+// let weatherToday = "Cloudy"; // Use 'let' to assign a value to a variable
+// console.log(weatherToday);
+
+// weatherToday = "Sunny";
+// console.log(weatherToday);
+
+// const hoursInDay = 24; // Use const to assign a constant value
+// console.log(hoursInDay);
+
+// hoursInDay = 12; // This will cause an error as we are trying to assign a value to a constant that has already got a value attached.
+// console.log(hoursInDay);
+
+
+// let a = 5;
+// let b = 3;
+// let result = a + b;
+// console.log(result);
+
+
+let name = "Jody";
+console.log("My name is " + name + " and I love to code") // These two lines of code will produce the same result 
+console.log(`My name is ${name} and I love to code`)      
